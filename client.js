3f12216e976cb35fa7030f326ee476dfe0b815d3
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-08-09 23:46:34 -1000

    server.js complete, need to add GET request for client.js

--- client.js
+++ client.js
@@ -1,7 +1,11 @@
 const net = require("net");
 const client = net.createConnection(8080, () => {
-  //   client.write("hello, i am A CLIENT!");
-  //   client.write('GET'+path+'HTTP/1.1\nhost: '+host);
+  //DO THIS ON CLIENT:
+  //Use process.argv to get the host/path you entered in in terminal (ie: node client.js localhost:8080/blahblahblah)
+  //1. Split the string into host and path and store it in variables
+  //2. concat the host and path
+  //3. then client write it
+
   client.on("data", data => {
     console.log(data.toString());
   });
