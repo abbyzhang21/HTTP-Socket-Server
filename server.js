e5ce74ceddb8e746f425836d286d57716089bcb6
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-08-09 20:38:27 -1000

    working on server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-<<<<<<< HEAD
-// HEy I  am writing new code... is it gonna delete yours?
-=======
-//this is from abby**********************
->>>>>>> 3695fb8c053bb5ff1e6f0e45c4addbc2d9c190b8
+//load the TCP library
+const net = require("net");
+//keep track of the chat clients
+const clients = [];
+//start a TCP Server
+const server = net.createServer(socket => {
+  //Identify this client
+  socket.name = socket.remoteAddress + ":" + socket.remotePort;
+  clients.push(socket);
+});
