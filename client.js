10e2145881a8c615d15d00b428e1f01c2fca8bc4
Abby Zhang <abbyzhang@Abbys-MacBook-Pro.local>
2018-08-10 21:39:33 -1000

    ...

--- client.js
+++ client.js
@@ -1,11 +1,17 @@
 const net = require("net");
-const client = net.createConnection(8080, () => {
-  //DO THIS ON CLIENT:
-  //Use process.argv to get the host/path you entered in in terminal (ie: node client.js localhost:8080/blahblahblah)
-  //1. Split the string into host and path and store it in variables
-  //2. concat the host and path
-  //3. then client write it
 
+console.log("\n\nCL process.argv =");
+console.log(process.argv);
+
+let header = "GET /apply HTTP/1.1\n\nhost: localhost:8080";
+// let slashIndex = process.argv[2].indexOf("/");
+// let host = process.argv[2].slice(0, slashIndex);
+// let path = process.argv[2].slice(slashIndex);
+
+const client = net.createConnection(8080, "0.0.0.0", () => {
+  client.write(header);
+  // client.write("GET /apply HTTP/1.1\nhost: localhost:8080");
+  // client.write("GET " + path + " HTTP/1.1\nhost: " + host);
   client.on("data", data => {
     console.log(data.toString());
   });
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-//load the TCP library
 const net = require("net");
-//start a TCP server
+
+console.log("\n\nCL process.argv");
+console.log(process.argv);
+let header = "\n\nHTTP/1.1 200 OK\n\nServer: SPARTASERVE";
+
+let clients = [];
+
 const server = net.createServer(client => {
-  client.write("Aloha");
+  client.write(header);
+
+  console.log("\nCLIENT CONNECTED\n");
+
+  client.write("\n\nWelcome to SpartaServe");
+
   client.on("data", data => {
     console.log(data.toString());
-    data.toString();
+    let msg = data.toString();
   });
+
+  clients.push(client);
 });
+
 server.listen(8080, () => {
-  console.log("Server listening on port 8080");
+  console.log("\nServer listening on port 8080");
 });
